7fcc48a24373a3cc6c1a1e8769e4a52c4d980c1b
sudeepp28 <122432690+sudeepp28@users.noreply.github.com>
2023-04-15 12:52:10 +0530

    milestone 3 added heroes functionality

--- angular-router-tour-of-heroes/src/app/app.module.ts
+++ angular-router-tour-of-heroes/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroListComponent } from './hero-list/hero-list.component';
+import { HeroListComponent } from './heroes/hero-list/hero-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
 
