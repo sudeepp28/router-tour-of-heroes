d9a71edf432d1b65f6cd013fc99d6fae1b47aa95
sudeepp28 <122432690+sudeepp28@users.noreply.github.com>
2023-04-15 12:09:26 +0530

    milestone 1

---
+++ angular-router-tour-of-heroes/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { RouterModule ,Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'heroes', component: HeroListComponent },
+  {path:'',redirectTo:'/heroes',pathMatch:'full'},
+  {path: '**',component:PageNotFoundComponent},
+
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    CrisisListComponent,
+    HeroListComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot( appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
